ea31019549341c7e74374c5221febc7c149ce2c9
Xaritoncat <sugrob.p.alex@gmail.com>
2026-09-04 20:49:43 +0300

    ввввввввв

---
+++ js.js
@@ -0,0 +1,7 @@
+let img_vk = document.querySelector(".img_vk")
+let img_tg = document.querySelector(".img_tg")
+let img_email = document.querySelector(".img_email")
+let img_github = document.querySelector(".img_github")
+img_vk.addEventListener("click",() => {
+    
+})
